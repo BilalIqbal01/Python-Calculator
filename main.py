17222618c8da7068a8d8f49e449ed7565af72f8d
BilalIqbal01 <168068404+BilalIqbal01@users.noreply.github.com>
2024-08-28 10:28:27 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+print("                            Welcome T0 Bilal Calculator")
+firstnumber=int(input("inter first number:"))
+secondnumber=int(input("inter second number:"))
+operator=(input("which operation you have to perform:"))
+if operator =="+":
+ print("Addition Answer=",firstnumber+secondnumber)
+if operator =="-":
+  print("Subtraction Answer=",firstnumber-secondnumber)
+if operator =="*":
+  print("Multiplication Answer=",firstnumber*secondnumber)
+if operator =="/":
+  print("Division Answer=",firstnumber/secondnumber)
+if operator =="**":
+  print("Power of two nimbers is=",firstnumber**secondnumber)   
+if operator =="%":
+  print("Reminder Answer=",firstnumber%secondnumber)
+if operator =="//":
+  print("Modules Answer=",firstnumber//secondnumber)
+ else:
+  print("Invalid operation")
+
+print("                          Thanks for using our calculator") 
+ 
+
+
