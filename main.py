5fc3fee49edb2b19b5a4e7dc161544188d881377
BilalIqbal01 <168068404+BilalIqbal01@users.noreply.github.com>
2024-08-28 12:02:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-print("                            Welcome T0 Bilal Calculator")
+print("                            ***Welcome T0 Bilal Calculator***")
 firstnumber=int(input("inter first number:"))
 secondnumber=int(input("inter second number:"))
 operator=(input("which operation you have to perform:"))
+
 if operator =="+":
  print("Addition Answer=",firstnumber+secondnumber)
-if operator =="-":
+ 
+elif operator =="-":
   print("Subtraction Answer=",firstnumber-secondnumber)
-if operator =="*":
+ 
+elif operator =="*":
   print("Multiplication Answer=",firstnumber*secondnumber)
-if operator =="/":
+ 
+elif operator =="/":
   print("Division Answer=",firstnumber/secondnumber)
-if operator =="**":
-  print("Power of two nimbers is=",firstnumber**secondnumber)   
-if operator =="%":
+ 
+elif operator =="**":
+  print("Power of two nimbers is=",firstnumber**secondnumber)  
+ 
+elif operator =="%":
   print("Reminder Answer=",firstnumber%secondnumber)
-if operator =="//":
+ 
+elif operator =="//":
   print("Modules Answer=",firstnumber//secondnumber)
- else:
+ 
+else:
   print("Invalid operation")
 
 print("                          Thanks for using our calculator") 
